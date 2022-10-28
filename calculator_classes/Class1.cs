761652b0715c2acd330b07475d602a8642df1919
evodka <lexaeva12@gmail.com>
2022-10-28 16:31:32 +0300

    first commit

---
+++ calculator_classes/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator_classes
+{
+    class Calculator
+    {
+        public int arv1;
+        public int arv2;
+        private string tehe;
+        public static int history = 0;
+
+
+        public Calculator(int aArv1, int aArv2, string aTehe)
+        {
+            arv1 = aArv1;
+            arv2 = aArv2;
+            Tehe = aTehe;
+            history++;
+        }
+
+
+        public string Tehe
+        {
+            get { return tehe; }
+            set
+            {
+                if (value == "+" || value == "-" || value == "*" || value == "/" )
+                {
+                    tehe = value;
+                }
+                else
+                {
+                    throw new ApplicationException("Not supported operation");
+                }
+            }
+        }
+    }
+}
---
+++ calculator_classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace calculator_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arv1 = 1;
+            int arv2 = 4;
+            string tehe = "+";
+            Calculator calc = new Calculator(arv1, arv2, tehe);
+        }
+    }
+}
